596c6ecf4779f11032373d5311136d9112ff87ee
Madona Abbott <happy_mikay1985@yahoo.com>
2022-03-25 16:48:55 -0400

    genpassword

--- script.js
+++ script.js
@@ -19,14 +19,15 @@ genBtn.addEventListener("click", buttonHandler);
 
 // ========= ⬆ DO NOT TOUCH THIS CODE ⬆ ======
 
-let password = document.getElementById("password");
-let chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
+const chars = [0123456789 abcdefghijklmnopqrstuvwxyz!@# $ % ^ & * () ABCDEFGHIJKLMNOPQRSTUVWXYZ];
+
+// create a global variable called "pwLength" with a number between 10 and 18
 const pwLength = 12;
 
-
+// Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
 function addNewPassword() {
     for (let i = 0; i < pwLength; i++) {
         let randomNumber = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNumber, randomNumber + 1);
     };
 };
