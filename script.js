1a0407ada650ea5a15da1c86c4e1cdd5b8fa31f0
Madona Abbott <happy_mikay1985@yahoo.com>
2022-03-25 16:04:49 -0400

    edit gitignore and script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const_ = require('lodash');
+
+// Start your code ***HERE*** =========
+
+// create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
+
+// create a global variable called "pwLength" with a number between 10 and 18
+
+// Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
+
+// ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
+
+let genBtn = document.getElementById("btnGen");
+let buttonHandler = () => {
+    document.getElementById("password").value = addNewPassword();
+};
+// Event listener for generate PW button
+genBtn.addEventListener("click", buttonHandler);
+
+// ========= ⬆ DO NOT TOUCH THIS CODE ⬆ ======
+
+let password = document.getElementById("password");
+let chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const pwLength = 12;
+
+
+function addNewPassword() {
+    for (let i = 0; i < pwLength; i++) {
+        let randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    };
+};
